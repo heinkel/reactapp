df97c6c052fc883ba134f103bd3c75b410d919f1
thazin <thazin.winlae@gmail.com>
2019-04-01 00:57:50 +0630

    Updates

---
+++ precache-manifest.f8a5d0023592ff0af288ca7fffc67a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "de9345c9e63cda2a84cc",
+    "url": "/reactapp/static/js/main.3e11ae4d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactapp/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "de9345c9e63cda2a84cc",
+    "url": "/reactapp/static/css/main.5acb6ca4.chunk.css"
+  },
+  {
+    "revision": "9d8db4ce53590478d4e25f288c1c34f3",
+    "url": "/reactapp/index.html"
+  }
+];
